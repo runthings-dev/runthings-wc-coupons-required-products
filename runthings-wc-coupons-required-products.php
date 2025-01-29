374818776de011f074df96ca889f1b058a53cf50
Matthew Harris <rtpharry@hotmail.com>
2025-01-29 10:28:00 +0000

    docs(meta): update php version to a supported version

--- runthings-wc-coupons-required-products.php
+++ runthings-wc-coupons-required-products.php
@@ -10,7 +10,7 @@
  * License: GPLv3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Requires at least: 6.4
- * Requires PHP: 7.4
+ * Requires PHP: 8.0
  * Requires Plugins: woocommerce
  * Text Domain: runthings-wc-coupons-required-products
  * Domain Path: /languages
