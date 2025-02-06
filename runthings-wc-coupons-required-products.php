5469fa85d71e25fef98681f86a7ca299d6288c9f
Matthew Harris <rtpharry@hotmail.com>
2025-02-06 14:34:46 +0000

    refactor: change namespace based on plugin dir feedback

--- runthings-wc-coupons-required-products.php
+++ runthings-wc-coupons-required-products.php
@@ -33,7 +33,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-namespace Runthings\WCCouponsRequiredProducts;
+namespace Runthings_WCCRP\WCCouponsRequiredProducts;
 
 use Exception;
 use WC_Coupon;
