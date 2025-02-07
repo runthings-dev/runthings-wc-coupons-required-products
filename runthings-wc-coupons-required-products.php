4cb9bb7f96acf7948fdd43d2eeb3918673be7f51
Matthew Harris <rtpharry@hotmail.com>
2025-02-07 00:25:26 +0000

    refactor: change namespace to match plugin name

--- runthings-wc-coupons-required-products.php
+++ runthings-wc-coupons-required-products.php
@@ -33,7 +33,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-namespace Runthings_WCCRP\WCCouponsRequiredProducts;
+namespace Runthings_WC_Coupons_Required_Products;
 
 use Exception;
 use WC_Coupon;
